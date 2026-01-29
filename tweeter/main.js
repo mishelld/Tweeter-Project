5be3db143726a91085f3898d9997c5f6200a79b3
mishelld <mishelldub@gmail.com>
2026-01-29 15:01:06 +0200

    feat: Implement initial client-side Tweeter application with core post and comment management.

--- tweeter/main.js
+++ tweeter/main.js
@@ -6,3 +6,40 @@ const renderer = Renderer();
 
 // This should render the initial dummy data
 renderer.renderPosts(tweeter.getPosts());
+
+const twit_button = document.querySelector("#twit-btn");
+const twit_input = document.querySelector("#twit-input");
+twit_button.addEventListener("click", () => {
+  const text = twit_input.value;
+  tweeter.addPost(text);
+  renderer.renderPosts(tweeter.getPosts());
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-post")) {
+    const delete_post = e.target;
+    const post_id = delete_post.id;
+    tweeter.removePost(post_id);
+    renderer.renderPosts(tweeter.getPosts());
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("comment-button")) {
+    const comment_btn = e.target;
+    const post_id = comment_btn.id;
+    const input = document.querySelector(`#${post_id}.comment-input`);
+    tweeter.addComment(post_id, input.value);
+    renderer.renderPosts(tweeter.getPosts());
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-comment")) {
+    const delete_comment = e.target;
+    const comment_id = delete_comment.dataset.id1;
+    const post_id = delete_comment.dataset.id2;
+    tweeter.removeComment(post_id, comment_id);
+    renderer.renderPosts(tweeter.getPosts());
+  }
+});
--- tweeter/render.js
+++ tweeter/render.js
@@ -1,7 +1,7 @@
 const Renderer = function () {
   function renderPosts(posts) {
     const posts_elem = document.querySelector("#posts");
-    posts.innerHTML = "";
+    posts_elem.innerHTML = "";
     posts.forEach((p) => {
       const post = document.createElement("div");
 
@@ -11,18 +11,31 @@ const Renderer = function () {
       post_text.classList.add("post-text");
       post_text.innerText = p.text;
 
-      const delete_post = document.createElement("div");
+      const delete_post = document.createElement("button");
       delete_post.classList.add("delete-post");
       delete_post.id = p.id;
       delete_post.innerText = "Delete Post";
       const comments = document.createElement("div");
       comments.classList.add("comments");
-      renderComments(comments, p.comments);
-      post.append(post_text, delete_post, comments);
+      renderComments(p.id, comments, p.comments);
+      const div = document.createElement("div");
+      div.classList.add("row");
+
+      const comment_input = document.createElement("input");
+      comment_input.classList.add("comment-input");
+      comment_input.id = p.id;
+      comment_input.placeholder = "Got something to say?";
+      const comment_btn = document.createElement("button");
+      comment_btn.classList.add("comment-button");
+      comment_btn.innerText = "Comment";
+      comment_btn.id = p.id;
+      div.append(comment_input, comment_btn);
+      post.append(post_text, comments, div, delete_post);
 
       posts_elem.appendChild(post);
     });
-    function renderComments(comments_elem, comments) {
+
+    function renderComments(post_id, comments_elem, comments) {
       comments.forEach((c) => {
         const comment = document.createElement("div");
         comment.classList.add("comment");
@@ -30,8 +43,14 @@ const Renderer = function () {
         comment.innerText = c.text;
         const delete_comment = document.createElement("div");
         delete_comment.classList.add("delete-comment");
-        delete_comment.id = c.id;
-        comments_elem.append(comment, delete_comment);
+        delete_comment.dataset.id1 = c.id;
+        delete_comment.dataset.id2 = post_id;
+
+        delete_comment.innerText = "X";
+        const div = document.createElement("div");
+        div.classList.add("row");
+        div.append(delete_comment, comment);
+        comments_elem.append(div);
       });
     }
   }
